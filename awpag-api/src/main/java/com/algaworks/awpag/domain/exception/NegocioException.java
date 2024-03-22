e55d063891c828864a2b680ebf2a4d4aaf861f63
Arthur Talles <atalles2005@gmail.com>
2024-03-22 11:01:36 -0300

    API parcelamento_cliente

---
+++ awpag-api/src/main/java/com/algaworks/awpag/domain/exception/NegocioException.java
@@ -0,0 +1,8 @@
+package com.algaworks.awpag.domain.exception;
+
+public class NegocioException extends RuntimeException{
+
+    public NegocioException(String message) {
+        super(message);
+    }
+}
